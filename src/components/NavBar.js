eafd7a95f13f632d9f5a3aca968a222e5a6ca007
Leestan360 <stanleymayore2@gmail.com>
2022-08-04 22:19:41 +0300

    Add navigation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
 
   return (
-    <div className="flex justify-between px-6 py-2 text-4xl text-white bg-orange-500">
-      <h3 className="">Stanley Mayore</h3>
+    <div className="flex justify-between px-6 py-2 text-3xl text-white bg-orange-500 font-[Poppins]">
+      <h3 className="py-0.5 text-4xl">Stanley Mayore</h3>
       <div className="">
-        <NavLink to="/" className="border border-black rounded-md px-1 py-0.5">Home</NavLink>
-        <NavLink to="/about" className="ml-4 border border-black rounded-md px-1 py-0.5">About</NavLink>
-        <NavLink to="/skills" className="ml-4 border border-black rounded-md px-1 py-0.5">Skills</NavLink>
-        <NavLink to="/projects" className="ml-4 border border-black rounded-md px-1 py-0.5">Projects</NavLink>
-        <NavLink to="/experience" className="ml-4 border border-black rounded-md px-1 py-0.5">Experience</NavLink>
-        <NavLink to="contact_me" className="ml-4 border border-black rounded-md px-1 py-0.5">Contact Me</NavLink>
+        <NavLink to="/" className="px-1 py-0.5">Home</NavLink>
+        <NavLink to="/about" className="ml-4 px-1 py-0.5">About</NavLink>
+        <NavLink to="/skills" className="ml-4 px-1 py-0.5">Skills</NavLink>
+        <NavLink to="/projects" className="ml-4 px-1 py-0.5">Projects</NavLink>
+        <NavLink to="/experience" className="ml-4 px-1 py-0.5">Experience</NavLink>
+        <NavLink to="contact" className="ml-4 px-1 py-0.5">Contact</NavLink>
       </div>
     </div>
   );
